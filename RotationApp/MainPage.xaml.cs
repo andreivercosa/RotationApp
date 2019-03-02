9c0c29473c9484e6ee8d73086cedfc87dcdf0d67
andreivercosa <andreivercosa@gmail.com>
2019-03-01 21:21:44 -0400

    v1

---
+++ RotationApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RotationApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int i =0;
+
+        void aumentar ( object sender, EventArgs e)
+        {
+            i++;
+            texto.Rotation = i;
+        }
+        void diminuir(object sender, EventArgs e)
+        {
+            i--;
+            texto.Rotation = i;
+        }
+    }
+}
